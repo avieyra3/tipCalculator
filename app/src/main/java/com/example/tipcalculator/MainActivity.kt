65a0bc0cd89d56322081f8187fd8e985b3d01165
Alfonso <a.vieyra@hotmail.co.uk>
2023-10-09 22:32:39 -0700

    Completed app!

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -7,42 +7,59 @@ import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import org.w3c.dom.Text
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Retrieve the amount
+        // Retrieve input data
         val etBillAmount: EditText = findViewById(R.id.billAmount)
-        var etBillAmountTxt: String = etBillAmount.text.toString()
+        val seekBar: SeekBar = findViewById(R.id.slider)
 
-        // wait until an amount has been entered
-        while(etBillAmountTxt.isEmpty()){
+        // Retrieve objects for displaying data
+        val displayTipPercentage: TextView = findViewById(R.id.tipPercentage)
+        val displayTipAmt: TextView = findViewById(R.id.tipAmt)
+        val displayTotalAmt: TextView = findViewById(R.id.totalAmt)
 
-        }
+        var billAmount: Double = 0.0
 
-        var billAmount: Double = etBillAmountTxt.toDouble()
+        etBillAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-        //Displayed percentage
-        var displayTipPercentage: TextView = findViewById(R.id.tipPercentage)
-        var seekBar: SeekBar = findViewById(R.id.slider)
-        var tip: Double = 0.0
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                displayTipPercentage.text = "$progress% "
-                tip = (progress * 0.01) * billAmount
-
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+            override fun afterTextChanged(s: Editable?) {
+                // Update billAmount and UI when text changes
+                billAmount = s?.toString()?.toDoubleOrNull() ?: 0.0
+                updateUI(billAmount, seekBar.progress, displayTipPercentage, displayTipAmt, displayTotalAmt)
             }
         })
 
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Update UI when SeekBar changes
+                updateUI(billAmount, progress, displayTipPercentage, displayTipAmt, displayTotalAmt)
+            }
 
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
 
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+    }
+
+    private fun updateUI(billAmount: Double, progress: Int, displayTipPercentage: TextView, displayTipAmt: TextView, displayTotalAmt: TextView) {
+        val tipPercentage = progress
+        val tip = (tipPercentage * 0.01) * billAmount
+        val total = billAmount + tip
+
+        val formattedTip = "%.2f".format(tip)
+        val formattedTotal = "%.2f".format(total)
+        // Update UI
+        displayTipPercentage.text = "$tipPercentage% "
+        displayTipAmt.text = "$formattedTip"
+        displayTotalAmt.text = "$formattedTotal"
     }
 }
